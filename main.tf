c1cfc947368d72a31ec92eeac78b4e86f8236c7e
mukesh-muthukumar <mukeshmmk384@gmail.com>
2026-07-17 12:09:30 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "testmukeshmuthu" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "test-tf"
     Environment = "Dev"
   }
 }
 
 
 resource "aws_instance" "example" {
+  ami           = "ami-02fc2ed964fe7933b"
   instance_type = "t3.micro"
+
+  subnet_id = "subnet-058eac748622c7497"
 
   tags = {
     Name = "HelloWorld"
