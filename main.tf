5b5bd635ae623ff878420b51314d8f8c7d6e2cbf
Mukesh <mukeshmmk384@gmail.com>
2026-08-05 14:57:03 +0530

    alias

--- main.tf
+++ main.tf
@@ -1,13 +1,30 @@
 terraform {
-    required_version = "< 1.15.9"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = ">= 3.0"
-        }
+  required_version = "< 1.15.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
+  alias  = "nv"
 }
+
+provider "aws" {
+  region = "ap-south-1"
+  alias  = "mumbai"
+}
+
+resource "aws_vpc" "test-vpc" {
+  cidr_block = "10.0.0.0/16"
+  provider   = aws.nv
+}
+
+resource "aws_vpc" "test-vpc-mumbai" {
+  cidr_block = "10.0.0.0/16"
+  provider   = aws.mumbai
+}
+
