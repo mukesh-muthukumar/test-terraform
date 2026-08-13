84e564468c4cc2b8dff3bba878eae07eaff6af73
Mukesh <mukeshmmk384@gmail.com>
2026-08-13 12:49:51 +0530

    update-for-each

--- main.tf
+++ main.tf
@@ -13,27 +13,16 @@ provider "aws" {
   alias  = "nv"
 }
 
-provider "aws" {
-  region = "ap-southeast-1"
-  alias  = "sing"
-}
-
-resource "aws_vpc" "test-vpc-nv" {
-  cidr_block = "10.0.0.0/16"
-  count = 3
-  provider   = aws.nv
-
-  tags = {
-    Name = "terraform-vpc-${count.index}"
+resource "aws_instance" "test-terraform" {  
+  ami           = "ami-0c55b159cbfafe1f0"
+  provider = aws.nv
+  for_each = {
+    dev = "t2.micro"
+    prod = "t2.large"
   }
-}
-
-resource "aws_vpc" "test-vpc-sing" {
-  cidr_block = "10.0.0.0/16"
-  count = 3
-  provider = aws.sing
 
 tags = {
-  Name = "terraform-vpc-${count.index}"
+  Name = "terraform-vpc-$each.key-${each.value}"
 }
 }
+
