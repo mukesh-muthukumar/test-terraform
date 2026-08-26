55fb33601efda1f80590eafa8c55f57533b6539c
Mukesh <mukeshmmk384@gmail.com>
2026-08-26 18:43:59 +0530

    changes

--- main.tf
+++ main.tf
@@ -13,30 +13,8 @@
     alias  = "nv"
   }
 
-  resource "aws_vpc" "nv_vpc" {
-    provider = aws.nv
-    cidr_block = "11.0.0.0/16"
-    tags = {
-      Name = "nv_vpc"
-    }
+  resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0"
+    instance_type = "var.instance_type"
+    provider      = aws.nv
   }
-
-  resource "aws_s3_bucket" "nv-s3-bucket-12387" {
-    provider = aws.nv
-    bucket   = "nv-s3-bucket-12387"
-    depends_on = [
-      aws_vpc.nv_vpc
-      ]
-    tags = {
-      Name = "nv_s3_bucket"
-    }
-  }
-
-resource "aws_s3_bucket_acl" "nv_s3_bucket_acl" {
-  provider = aws.nv
-  bucket = aws_s3_bucket.nv-s3-bucket-12387.id
-  acl    = "private"
-  depends_on = [
-    aws_s3_bucket.nv-s3-bucket-12387
-  ]
-}
